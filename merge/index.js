760be0bdf2f2316d0a00572495f97661ff8d08dd
Duncan Grubbs <duncan.grubbs@gmail.com>
2018-01-19 16:11:35 -0500

    Fix pop-up auth.

--- merge/index.js
+++ merge/index.js
@@ -17,13 +17,13 @@ const $signIn = document.getElementById("signIn");
 // Sign in:
 // TODO: the sign-in window does not pop up; delete unnecessary comments
 $signIn.addEventListener("click", function () {
-    /*var provider = new firebase.auth.GoogleAuthProvider();
+    var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function (result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
-        // ...
+        window.location.href = '/pages/posts.html';
     }).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
@@ -33,11 +33,6 @@ $signIn.addEventListener("click", function () {
         // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
         // ...
-    });*/
-    const auth = firebase.auth();
-    const promise = auth.signInWithEmailAndPassword("123@gmail.com", "123456");
-    promise.catch(function (error) {
-        console.log(error);
     });
     //window.location.replace("pages/posts.html")
 });
